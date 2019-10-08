c8743fc9dbe3a93cc89374293d805460068c3d80
Cody Dullard <44715314+CodyDullard@users.noreply.github.com>
2019-10-08 14:10:11 +0100

    Update stack.py

--- Examples/stack.py
+++ Examples/stack.py
@@ -1,8 +1,10 @@
 #This is a basic Stack data structure usin OOP programming in python.
 #A stack is a data structure which uses a LIFO(Last In First Out) order to preform operations.
+#Last In First Out means that the last item to be added to the stack will be the first one to be taken out
 #It is used in many cases. For example internet search history and creating programming languages.
 #This program is a bare bones version but can be improved and added to
-
+#Python implements a Stack by using a list. 
+#The list contains all the information held in the stack and we use python list functions within our stack methods.
 class Stack(object):
 
     def __init__(self):
