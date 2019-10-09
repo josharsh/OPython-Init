d5d7a99da542104ec2f8658dfedfa0ce7cdbb585
ShambhaviCodes <shambhavimishra26@gmail.com>
2019-10-09 07:46:21 +0530

    Added example code for Binary Search

---
+++ Examples/Binary_Search.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 07:36:45 2019
+
+@author: hp
+"""
+
+# Returns index of x in arr if present, else -1 
+def binarySearch (arr, l, r, x): 
+  
+    # Check base case 
+    if r >= l: 
+  
+        mid = l + (r - l)/2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+          
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binarySearch(arr, mid+1, r, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
