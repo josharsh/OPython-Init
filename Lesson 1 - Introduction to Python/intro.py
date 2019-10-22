11838daed6ecfc18806f4eccd404639ef09c3ebe
Swarnabha Roy <scsr2448@gmail.com>
2019-10-22 20:27:47 +0530

    Create intro.py
    Updates intro.py

---
+++ Lesson 1 - Introduction to Python/intro.py
@@ -0,0 +1,54 @@
+'''
+Beginning with Python programming:
+
+Finding an Interpreter:
+
+Before we start Python programming, we need to have an interpreter to interpret and run our programs. 
+There are certain online interpreters like http://ideone.com/ or http://codepad.org/  that can be used to start Python 
+without installing an interpreter.
+
+Windows:There are many interpreters available freely to run Python scripts like IDLE ( Integrated Development Environment ) 
+which is installed when you install the python software from http://python.org/
+
+Linux:For Linux, Python comes bundled with the linux.
+
+'''
+#first program in Python
+# Script Begins 
+  
+print("Hello!!") 
+  
+# Scripts Ends 
+'''
+OUTPUT
+Hello!!
+'''
+'''
+NOTE
+In a Python script to print something on the console print() function is used – it simply prints out a line ( and also 
+includes a newline unlike in C ). One difference between Python 2 and Python 3 is the print statement. In Python 2, the “print” 
+atement is not a function, and therefore can be invoked without a parenthesis. However, in Python 3, it is a function, and must 
+invoked with parentheses.
+'''
+'''
+A suggestion would be to visit the documentation page of Python and study the keywords, functions, classes before actually
+going ahead with python programming.
+'''
+#a second program to perform basic arithmetic operations
+a = 10
+b = 20
+c = a+b
+d = c-a
+e = d*c
+f = d/b
+print(c)
+print(d)
+print(e)
+print(f)
+'''
+OUTPUT
+30
+20
+600
+1.0
+'''
