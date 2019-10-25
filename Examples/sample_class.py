48cbbfee17de95250b1e1b2df390d4ca9d15e2dc
Gnanesh Dommeti <gnanesh.dometti@gmail.com>
2019-10-25 23:59:59 +0530

    Creating a simple class

---
+++ Examples/sample_class.py
@@ -0,0 +1,33 @@
+# creating class
+class Sparrow:
+
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # instance method
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
+# instantiate the Sparrow class
+blu = Sparrow("Blu", 10)
+woo = Sparrow("Woo", 15)
+
+# access the class attributes
+print("Blu is a {}".format(blu.__class__.species))
+print("Woo is also a {}".format(woo.__class__.species))
+
+# access the instance attributes
+print("{} is {} years old".format( blu.name, blu.age))
+print("{} is {} years old".format( woo.name, woo.age))
+
+# call our instance methods
+print(blu.sing("'Happy'"))
+print(blu.dance())
