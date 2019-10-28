431a844f7ae26c5b3dc9a3421e2d3d760a693149
Shinvalor <hpoon16@huntersoe.org>
2019-10-28 19:33:46 -0400

    Basic of Python Example in Lesson 1

---
+++ Lesson 1 - Introduction to Python/intro.py
@@ -0,0 +1,19 @@
+# Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# String Operations
+a = "Hello"
+b = "World"
+print(a+b)
+
+# For loop travesing a list
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# For loop iterating 6 times
+for i in range(6):
+  print(i)
