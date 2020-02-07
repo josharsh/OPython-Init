dab0a6346edc29a618013c39d5c674b4e4a2611a
Rotherford Marmibe <rmarmibe@gmail.com>
2020-02-08 00:16:55 +0800

    added samples for arguments in functions example

--- Lesson 2 - Using Functions/Function.py
+++ Lesson 2 - Using Functions/Function.py
@@ -12,8 +12,31 @@ yello("Yepp")
 
 
 #############################################################
-
+#Creating a function that takes keyword arguments
 def hey(phrase = "Gooden Morden"):
     print(phrase, '!')
     
-hey()    
+hey()
+
+#############################################################
+# Creating a function that takes a variable number of positional arguments
+def varargs(*args):
+    return(args)
+
+arguments = varargs('argument1', 'argument2', 'argument3')
+print(arguments[0])
+print(arguments[1])
+print(arguments[2])
+
+#############################################################
+# Creating a function that takes a variable number of positional arguments
+# and keyword arguments
+def both_the_args(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+args = (1, 2, 3, 4, 5, 6)
+kwargs = {"A": 7, "B": 8}
+both_the_args(*args)
+both_the_args(**kwargs)
+both_the_args(*args, **kwargs)
