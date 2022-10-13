ddd47d7693803553383e2dcf68bfdcd53790d2ff
brianjtarte <brianjtarte@gmail.com>
2022-10-12 20:37:34 -0700

    Reformatted some explanations in 'FormatStringInput' to better describe what the methods do. In addition, added the .capwords() method to show how that can be utilized as well.

--- Lesson 6 - String Manipulation/FormatStringInput.py
+++ Lesson 6 - String Manipulation/FormatStringInput.py
@@ -1,7 +1,22 @@
-favColor = input("What is your favorite color:-").upper()   #makes every letter CAPITAL
+import string  # make sure that you import string to utilize the .capwords() method
+
+favColor = input("What is your favorite color?:-").upper()  # makes every letter CAPITAL
 print(favColor)
 
-#or
+# or
 
-favColor = input("What's your favorite color:-")
-print(favColor.capitalize())        #makes first letter Capital
+favColor = input("What's your favorite color?:-")
+print(favColor.capitalize())  # will capitalize first character and convert all other characters to lower case. Does
+# not work if the first character is not a letter (empty space, number, etc.). See example below:
+
+capitalizeExample = "the quick brown FOX jumps ovEr the lazy DOG"  # this will print when you run the file, but take note of the formatting
+print('This is the string prior to calling the .capitalize() method on it: -->',
+      capitalizeExample)  # this is the string
+# prior to calling the capitalize method on it
+print('This is what happens after calling .capitalize() on the string: -->', capitalizeExample.capitalize())
+
+# or
+
+favColor = input("What's are your favorite colors?:-")
+print(string.capwords(
+    favColor))  # utilize string.capwords() method from Python library to make the first letter of every word capitalized
